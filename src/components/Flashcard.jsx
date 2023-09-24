446314958357404ebf18130978548514182f96d3
Salemmander <salemnassar5@gmail.com>
2023-09-24 05:54:31 -0500

    implemented switching of question to answer

---
+++ src/components/Flashcard.jsx
@@ -0,0 +1,18 @@
+import "../App.css";
+import { useState } from "react";
+
+const Flashcard = (props) => {
+    const [showAnswer, setshowAnswer] = useState(false);
+
+    const toggleAnswer = () => {
+        setshowAnswer(!showAnswer);
+    };
+
+    return (
+        <div onClick={toggleAnswer} className='flashcard'>
+            <h1>{showAnswer ? props.card["a"] : props.card["q"]}</h1>
+        </div>
+    );
+};
+
+export default Flashcard;
